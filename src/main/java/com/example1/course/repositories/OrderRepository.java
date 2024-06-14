09b7f7a1a8b60fb04c08effab5dd68bc5d964ba2
Leonardo Siqueira <leo2017siqueira@outlook.com>
2024-06-14 11:31:59 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/example1/course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.example1.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example1.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+	
+
+}
